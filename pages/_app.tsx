4c8b86e97c84d2bdb46870dfc70c8236133351ba
Hao <prokilchu@gmail.com>
2024-12-04 10:11:25 -0800

    updated font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
+import { Noto_Sans } from 'next/font/google';
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
  
 export default function App({ Component, pageProps }: AppProps) {
-  return  <Component {...pageProps} />
+  return (
+    <main className={notoSans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
