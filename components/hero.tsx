a15fe05ec9750be2ffddde03651665b1e89ae20c
Yocodeyo <58803396+Yocodeyo@users.noreply.github.com>
2024-12-05 14:09:33 -0800

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,7 @@
 export function Hero() {
     return (
-        <><div className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white p-4 mt-8 flex items-center justify-center space-x-4">
-            <img src="/talkarena-logo.svg" alt="Logo" className="w-18 h-18" />
+        <><div className="text-slate-900 font-extrabold text-3xl sm:text-4xl lg:text-5xl tracking-tight text-center dark:text-white p-4 mt-8 flex items-center justify-center space-x-4">
+            <img src="/talkarena-logo.svg" alt="Logo" className="w-24 h-24" />
             Interactive Evaluation of Large Audio Models
         </div>
         <div className="text-slate-600 text-center dark:text-slate-400 p-4">
