5833de45997992e2b6c349716dc75bbe929c4b32
William Held <Wbh230@nyu.edu>
2024-12-09 16:25:20 -0800

    Update gradio.tsx

--- components/gradio.tsx
+++ components/gradio.tsx
@@ -33,7 +33,7 @@ export function GradioEmbed() {
         <div className="bg-white rounded-lg shadow-lg">
           <gradio-app 
             title="gradio demo" 
-            src="https://42adf08229af2e268c.gradio.live"
+            src="https://c3cdd7ea10c556e43d.gradio.live"
           />
         </div>
       </div>
