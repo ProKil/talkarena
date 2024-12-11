4dbb8943dc98831206213c5a9e93d1e8779adf5e
William Held <Wbh230@nyu.edu>
2024-12-10 17:23:21 -0800

    Actually Refresh Every 5 Min

--- components/leaderboard_viz.tsx
+++ components/leaderboard_viz.tsx
@@ -32,7 +32,7 @@ export function LeaderboardEmbed() {
       <div className="max-w-6xl mx-auto" style={{width: "min(900px, 98vw)", marginLeft: "calc((100% - min(900px, 98vw)) / 2)"}}>
         <gradio-app 
           title="gradio leaderboard" 
-          src="https://1977e4e3f512d7349e.gradio.live/"
+          src="https://dd9be0bed77694bd00.gradio.live"
         />
       </div>
   );
